67a80c7d583439e33c3100cd5a325adfc1908a7b
nurlan1234nur <nurlant566@gmail.com>
2026-04-19 01:14:04 +0800

    fix: remove floating TripleGeniusBadge button

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,12 @@ import { RouterProvider } from 'react-router';
 import { router } from './routes';
 import { LanguageProvider } from './contexts/LanguageContext';
 import { AuthProvider } from './contexts/AuthContext';
-import { TripleGeniusBadge } from './components/TripleGeniusBadge';
 
 export default function App() {
   return (
     <LanguageProvider>
       <AuthProvider>
         <RouterProvider router={router} />
-        <TripleGeniusBadge />
       </AuthProvider>
     </LanguageProvider>
   );
