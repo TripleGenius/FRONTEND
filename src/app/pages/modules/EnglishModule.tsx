c7a7161c2eb19e42cb7f0c4d1401ad0f344a82be
nurlan1234nur <nurlant566@gmail.com>
2026-04-19 18:07:08 +0800

    fix english module

--- src/app/pages/modules/EnglishModule.tsx
+++ src/app/pages/modules/EnglishModule.tsx
@@ -4,6 +4,6 @@ import { useLanguage } from '../../contexts/LanguageContext';
 export function EnglishModule() {
   const { t } = useLanguage();
   return (
-    <ModuleCard slug="english" title={t('module.english')} icon="🇬🇧" color="from-[#8b9d83] to-[#9faaa0]" />
+    <ModuleCard slug="english" title={t('module.english')} icon="🇬🇧" color="from-[#8b9d83] to-[#9faaa0]" randomize flipable />
   );
 }
